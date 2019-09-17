6da3dc999142fbe07838a2e4dc427c08f0c81ecf
Flo (on Moody Pi) <flo@moodypi.none>
2019-09-17 12:31:36 +0100

    Started cam view in Vue app (using vue splitter pane)

---
+++ src/src/components/LiveView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="liveview">
+    <vue-splitter-v>
+      <template slot="left">
+        <img src="http://192.168.1.115:8082/stream.mjpg" class="preview" />
+      </template>
+      <template slot="right">
+        <img src="http://192.168.1.115:8080/stream.mjpg" class="preview" />
+      </template>
+    </vue-splitter-v>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.VueSplitter-Handle-item {
+  background-color: green;
+  border: 1px solid red;
+  height: 100vh;
+}
+
+</style>
--- src/src/main.js
+++ src/src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueSplitter from "vue-splitter-pane";
 
 Vue.config.productionTip = false;
+Vue.use(VueSplitter);
 
 new Vue({
   render: h => h(App)
