a05dc7e2e340d22302ae8c71b478b0aa781cdeba
Flo (Numbercruncher) <info@florian-geiselhart.de>
2019-10-03 19:47:37 +0200

    * MaxMSP patch for sending mic volume from Ableton to local SIO proxy, then to light controller * Basic Ableton project for voice processing

---
+++ server/max_to_lights.js
@@ -0,0 +1,39 @@
+var osc = require('osc-min');
+var dgram = require('dgram');
+var io = require('socket.io-client');
+
+var remote;
+
+var masterServer = '192.168.2.192'; 
+var masterServerPort = 8099;
+
+var lowerLimit = -70;
+var upperLimit = 12;
+
+var socket = io.connect('http://'+masterServer+':'+masterServerPort, {reconnect: true});
+
+// Map helper
+function map(val, low1, high1, low2, high2) {
+  return (val - low1) / (high1 - low1) * (high2 - low2) + low2;
+}
+
+// Listen for OSC messages from Max and fwd them to Moodys light control
+var udp = dgram.createSocket('udp4', function(msg, rinfo) {
+
+  // save the remote address
+  remote = rinfo.address;
+
+  try {
+  	outMsg = osc.fromBuffer(msg)
+    //console.log(msg);
+    //Process and send to SIO
+    var level = map(outMsg.args[0].value, lowerLimit, upperLimit, 0, 100);
+    console.log(level);
+    socket.emit("broadcast", {method:"showlevel",payload:level});
+  } catch (err) {
+    console.log('Could not decode OSC message');
+  }
+
+});
+
+udp.bind(9898);
